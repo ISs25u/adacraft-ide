a673fcfd41b6fb7e4f287f5fb00a58cffc5f51e9
Raphael Pierquin <raphael@ut7.fr>
2015-03-24 21:52:34 +0100

    migrate to Flask and remove legacy auth files

--- main.py
+++ main.py
@@ -3,18 +3,13 @@
 #
 """
 Dependencies (on a Ubuntu computer, on other systems you will need to find other ways of finding these) :
-  python-bottle
-  python-beaker
-  python-pycrotopp (on Ubuntu 12.04, maybe not needed on 12.10 and newer)
-
-Included in this repo:
-  cork
+  Flask
 
 Loaded remotely:  
   ace   (javascript editor from ace.ajax.org)
   jquery 
- 
- 
+
+
  NB/TODO:
  - security issues in the handling of filenames (both file select and save)
 """
@@ -22,12 +17,13 @@ Loaded remotely:
 from __future__ import unicode_literals
 import io                   # Better alternative for unicode files.
 import cgi
-import bottle
-from bottle import route, static_file, template, redirect, request, post, get
-from beaker.middleware import SessionMiddleware
 import glob
 import string
 import datetime
+
+from flask import Flask, request
+
+app = Flask(__name__)
 
 ENCODING="UTF-8"
 
@@ -36,17 +32,13 @@ JSDIR = os.environ['SRCDIR']
 BKDIR = os.environ['SRCDIR'] +"-backups"
 ACEDIR = "content/ace"
 
-@route("/css/<filename>")
-def css_static(filename):
-    return static_file(filename, root="content/css")
-
 def get_html_file(fname):
     try:
         return io.open(fname, "r", encoding=ENCODING).read()
     except:
         return ""
 
-@route("/edit/")
+@app.route("/edit/")
 def edit():
     "lists files we might want to edit"
 
@@ -62,51 +54,40 @@ def edit():
     html = get_html_file("content/template/edit.html")
     return string.Template(html).substitute(flist = lis)
 
-@route("/edit/file")
+@app.route("/edit/file")
 def editfile():
     "lists files we might want to edit"
     #html = get_html_file("content/template/editfile.html")
-    fname = request.query.file
+    fname = request.args.get("file")
 
     html = get_html_file("content/template/editfile_ace.html")
     cont = get_html_file("%s/%s" % (JSDIR, fname))
     return string.Template(html).substitute(fname = fname, content = cgi.escape(cont))
 
 
-@route("/edit/ace/<filename>")
+@app.route("/edit/ace/<filename>")
 def edit_ace_file(filename):
     "serves the ace files"
     cont = get_html_file("%s/%s" % (ACEDIR, filename))
     return cont
 
-
-# Maybe not needed anymore
-@get('/edit/sfile')
-def editfile_hidden():
-    return ""
-
-@post('/edit/sfile')
+@app.route('/edit/sfile', methods = ['POST'])
 def editfile_submit():
-    "Handles 'save file'"
+    "Handles save file'"
     print "Submitting new file"
-    fname = request.forms['fname']
-    txt   = request.forms['text']
+    fname = request.form['fname']
+    txt   = request.form['text']
     print "  fname", fname, "len", len(txt)
-
+    return ""
     # create a backup
     tnow = datetime.datetime.now()
-    try:
-        oldtxt = io.open("%s/%s" %(JSDIR, fname), "r", encoding=ENCODING).read()
-        io.open("%s/%s-pre-%s" %(BKDIR, fname.replace('/', '-'), tnow.isoformat()), "w", encoding=ENCODING).write(oldtxt)
-    except:
-        pass
+    oldtxt = io.open("%s/%s" %(JSDIR, fname), "r", encoding=ENCODING).read()
+
     # Now overwrite the file we want to save
     io.open("%s/%s" %(JSDIR, fname), "w", encoding=ENCODING).write(unicode(txt,encoding = ENCODING))
-    
     return ""
 
-
-@route("/logfile")
+@app.route("/logfile")
 def logfile():
     html = get_html_file("content/template/logfile.html")
     cont = get_html_file("mcserver.log")
@@ -118,16 +99,5 @@ def logfile():
 #  Mainstuff
 # 
 
-# Get cork and bottle to cooperate
-session_opts = {
-    'session.type': 'cookie',
-    'session.validate_key': True,
-}
-
-app = bottle.default_app()
-app = SessionMiddleware(app, session_opts)
-
-# With reloader: automatically reloads the bottle server when the python modules have been updated. 
-#bottle.run(app=app, reloader=True, host="localhost", port=8090)
-bottle.run(app=app, reloader=True, host="0.0.0.0", port=8095)  # listens to all hosts
-
+if __name__ == "__main__":
+    app.run(debug=True)
