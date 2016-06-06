50ca2a96d8c60bceb20e2c7bccbc0743007c82c1
Jonathan Perret <jonathan@ut7.fr>
2016-06-06 16:35:09 +0200

    Fix auth tests and add a command-line to generate tokens

--- auth.py
+++ auth.py
@@ -1,4 +1,20 @@
 import jwt
 
+ALGORITHM = 'HS256'
+
+
 def verifyToken(token, secret):
-    return jwt.decode(token, secret, algorithm='HS256')
+    return jwt.decode(token, secret, algorithm=ALGORITHM)
+
+
+def createToken(data, secret):
+    return jwt.encode(data, secret, algorithm=ALGORITHM)
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        sys.stderr.write('usage: auth.py SECRET USER\n')
+        sys.exit(1)
+    secret = sys.argv[1]
+    sys.stdout.write(createToken(dict(login=sys.argv[1]), sys.argv[2]) + '\n')
