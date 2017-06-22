82c892db7b6cdc910725e588b333b2cbd9cbe2d3
Jonathan Perret <jonathan@ut7.fr>
2017-06-22 15:15:48 +0200

    Finish Droplet removal

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,6 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css$/, loader: "style-loader!css-loader" },
-
-      // Fix the mess in droplet-editor
-      { test: /quadtree\.js$/, loader: "exports-loader?QUAD" },
-      { test: /\.coffee$/, loader: "coffee-loader" },
-      { test: [/droplet-editor\/src\/languages\/.*\.coffee$/, /\/sax\//],
-        exclude: /droplet-editor\/src\/languages\/javascript.coffee$/,
-        enforce: 'post',
-        loader: "null-loader" },
     ]
   },
 };
