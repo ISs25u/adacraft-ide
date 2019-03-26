a529fb744e18b0b4fff6bfea938613e9133383dc
batlac <baptiste.lac@insa-lyon.fr>
2019-03-26 16:51:10 +0100

    Added schematic file transfert

--- main.py
+++ main.py
@@ -1,74 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-"""
-Dependencies (on a Ubuntu computer, on other systems you will need to find other ways of finding these) :
-  Flask
-
-Loaded remotely:
-  ace   (javascript editor from ace.ajax.org)
-  jquery
-
- NB/TODO:
- - security issues in the handling of filenames (both file select and save)
-"""
 
 from __future__ import unicode_literals
-import io                   # Better alternative for unicode files.
+import io 
 import glob
 import os
 import signal
 import mftp
 
 from flask import Flask, make_response, request, session, render_template, flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 ENCODING = "UTF-8"
-JSDIR = os.environ['LOC_WDIR']
 DEBUG = 'DEBUG' in os.environ
 SECRET = os.environ['SECRET']
-FILEMODE = os.environ['MODE']
-HOST = os.environ['LOC_HOST']
+HOST = os.environ['HOST']
+SCHEM_DIR = os.environ['FTP_SCHEM']
 
 def get_file_content(file_path):
 
-    playername, filename = file_path.split('/')
+  playername, filename = file_path.split('/')
 
-    try:
+  try:
+    return mftp.load_file(playername,filename)
 
-        if FILEMODE == 'SSH' :
-            return mftp.load_file(playername,filename)
-
-        else :
-	    with open("%s/%s"%(JSDIR,file_path),'r') as f :
-                return f.read()
-
-    except:
-        return ""
-
+  except:
+    return ""
 
 def set_file_content(file_path, txt):
 
-    playername, filename = file_path.split('/')
+  playername, filename = file_path.split('/')
 
-    if FILEMODE == 'SSH' :
-        mftp.save_file(playername, filename, txt)
-    else :
-        if not os.path.isdir(os.path.dirname("%s/%s"%(JSDIR,file_path))):
-            os.makedirs(os.path.dirname("%s/%s"%(JSDIR,file_path)))
-        
-        with open("%s/%s"%(JSDIR,file_path), 'w') as f :
-            f.write(txt)
+  mftp.save_file(playername, filename, txt)
+
+  return
+
 
 def logged_in_player():
-    if 'player' in session:
-        return session['player']
-    else:
-        return None
+
+  if 'player' in session:
+    return session['player']
+  else:
+    return None
 
 # ---------------------
 
 app = Flask(__name__)
 app.secret_key = SECRET
+app.config['UPLOAD_FOLDER'] = SCHEM_DIR
+app.config['MAX_CONTENT_LENGTH'] = 128 * 1024       # Max file size is 128 KiB
 
 @app.route('/')
 def index():
@@ -77,129 +58,146 @@ def index():
 
 @app.route('/auth/<token>')
 def auth(token):
-    import auth
-    try:
-        info = auth.verifyToken(token, SECRET)
-        login = info['login']
-        session['player'] = login
-        flash('Logged in as %s' % login)
-    except Exception, e:
-        app.logger.error("Auth error: %s" % str(e))
-        flash('Invalid token')
+  import auth
+  try:
+    info = auth.verifyToken(token, SECRET)
+    login = info['login']
+    session['player'] = login
+    flash('Logged in as %s' % login)
+  except Exception, e:
+    app.logger.error("Auth error: %s" % str(e))
+    flash('Invalid token')
 
-    return redirect(url_for('edit'))
+  return redirect(url_for('edit'))
 
 
 @app.route('/logout')
 def logout():
-    if 'player' in session:
-        session.pop('player', None)
-        flash('Logged out')
-    return redirect(url_for('edit'))
+  if 'player' in session:
+    session.pop('player', None)
+    flash('Logged out')
+  return redirect(url_for('edit'))
 
 
 @app.route("/edit/")
 def edit():
-    "lists files we might want to edit"
+  "lists files we might want to edit"
 
-    if FILEMODE == 'SSH' :
-        fnames = mftp.list_files()
-    else :
-        directories = glob.glob("%s/*/"%(JSDIR))
-        fnames = {}
-        for directory in directories:
-            files = glob.glob("%s*.js"%directory)
-            dirname = os.path.basename(os.path.dirname(directory))
-            if files :
-                fnames[dirname] = [os.path.basename(f) for f in files]
-  
-    ownedFiles = None
-    playerDir = None
-    if logged_in_player():
-        ownedFiles = []
-        playerDir = logged_in_player()
-        if playerDir in fnames:
-            ownedFiles = fnames[playerDir]
-            del fnames[playerDir]
-    return render_template(
-        'edit.html',
-        ownedFiles=ownedFiles,
-        playerDir=playerDir,
-        filesByDirectory=fnames,
-        logged_in_player=logged_in_player()
-    )
+  fnames = mftp.list_files()
+
+  ownedFiles = None
+  playerDir = None
+
+  if logged_in_player():
+    ownedFiles = []
+    playerDir = logged_in_player()
+    if playerDir in fnames:
+      ownedFiles = fnames[playerDir]
+      del fnames[playerDir]
+
+  return render_template(
+    'edit.html',
+    ownedFiles=ownedFiles,
+    playerDir=playerDir,
+    filesByDirectory=fnames,
+    logged_in_player=logged_in_player())
 
 
 @app.route("/schematic/")
-def editSchem():
+def schematic():
     
-    schemFiles = ["schem1.schematic","schem2.schematic","schem3.schematic"]
+  schemFiles = mftp.list_schematics();
 
-    return render_template('schematic.html', schemFiles=schemFiles, logged_in_player=logged_in_player())
+  return render_template('schematic.html', schemFiles=schemFiles, logged_in_player=logged_in_player())
+
 
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-       #f = request.files['the_file']
-       #f.save('/var/www/uploads/uploaded_file.txt')
-       print(request)
+def upload_schematic():
 
-    return "<span>ACAB</span>"
+  if request.method == 'POST':
+
+    if 'schemLoader' not in request.files :
+      flash('Pas de fichier trouvé', category='warning')
+      return redirect(url_for('schematic'))
+
+  mfile = request.files['schemLoader']
+
+  if mfile.filename == '':
+    flash('Pas de fichier sélectionné', category='warning')
+    return redirect(url_for('schematic'))
+
+  if mfile:
+ 
+    cur_files = mftp.list_schematics()
+    filename = secure_filename(mfile.filename)
+
+    if filename not in cur_files:
+
+      flash('Le ficher \''+ filename + '\' a été transféré', category='info') 
+      mpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+      mftp.save_schematic(filename, mfile)
+      return redirect(url_for('schematic'))
+
+    else :
+
+      flash('Le ficher \''+ filename + '\' existe déjà...', category='error') 
+      return redirect(url_for('schematic'))
+
 
 @app.route("/edit/<playername>/<filename>")
 def editfile(playername, filename):
-    "edit the content of a file"
+  "edit the content of a file"
 
-    fname = playername + '/' + filename
+  fname = playername + '/' + filename
 
-    app.logger.debug(url_for("load", playername=playername, filename=filename))
+  app.logger.debug(url_for("load", playername=playername, filename=filename))
 
-    return render_template(
-        'editfile_ace.html',
-        fname=fname,
-        logged_in_player=logged_in_player(),
-        can_save=logged_in_player() == playername,
-        save_url=url_for("save", playername=playername, filename=filename),
-        file_url=url_for("load", playername=playername, filename=filename),
-    )
+  return render_template(
+    'editfile_ace.html',
+    fname=fname,
+    logged_in_player=logged_in_player(),
+    can_save=logged_in_player() == playername,
+    save_url=url_for("save", playername=playername, filename=filename),
+    file_url=url_for("load", playername=playername, filename=filename),
+  )
 
 
 @app.route("/load/<playername>/<filename>")
 def load(playername, filename):
-    "get the content of a file"
+  "get the content of a file"
 
-    fname = playername + '/' + filename
-    mfile = get_file_content("%s"%fname)
+  fname = playername + '/' + filename
+  mfile = get_file_content("%s"%fname)
 
-    response = make_response(mfile.encode("utf8"))
-    response.headers['Content-Type'] = "text/plain"
-    response.headers['Content-Disposition'] = "inline; filename=" + filename
-    return response
+  response = make_response(mfile.encode("utf8"))
+  response.headers['Content-Type'] = "text/plain"
+  response.headers['Content-Disposition'] = "inline; filename=" + filename
+  return response
 
 
 @app.route('/save/<playername>/<filename>', methods=['GET', 'POST'])
 def save(playername, filename):
-    "Handles save file"
+  "Handles save file"
 
-    if logged_in_player() != playername:
-        return "", 403
+  if logged_in_player() != playername:
+    return "", 403
 
-    fname = playername + '/' + filename
-    app.logger.debug("Saving file %s" % fname)
+  fname = playername + '/' + filename
+  app.logger.debug("Saving file %s" % fname)
 
-    txt = request.form['text']
-    
-    file_path = "%s"%fname
-    set_file_content(file_path, txt)
+  txt = request.form['text']
+  
+  file_path = "%s"%fname
+  set_file_content(file_path, txt)
 
-    return ""
+  return ""
 
 
 def term_handler(signum, frame):
-    app.logger.info("Caught TERM signal, shutting down.")
-    exit(0)
+  app.logger.info("Caught TERM signal, shutting down.")
+  exit(0)
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGTERM, term_handler)
-    app.run(host=HOST, debug=DEBUG)
+  signal.signal(signal.SIGTERM, term_handler)
+  app.run(host=HOST, debug=DEBUG)
--- mftp.py
+++ mftp.py
@@ -9,115 +9,173 @@ WDIR = os.environ['FTP_WDIR']
 USR = os.environ['FTP_USER']
 PWD = os.environ['FTP_PASS']
 HST = os.environ['FTP_HOST']
+SCHEM = os.environ['FTP_SCHEM']
 
 session = None
 
 def getSession():
 
-    global session
+  global session
 
+  try :
+    session = FTP(HST)
+    session.login(USR,PWD)
+  except :
+    print("\tError connecting FTP")
 
-#   if session is None :
-#     print("\tFTP Connect")
-
-    try :
-      session = FTP(HST)
-      session.login(USR,PWD)
-    except :
-       print("\tError connecting FTP")
-
-    return session
+  return session
 
 def list_files():
+  
+  print("\tFTP list files")
 
-    print("\tFTP list files")
+  ses = getSession()
 
-    ses = getSession()
+  if ses is None :
+    return {}
 
-    if ses is None :
-      return {}
+  content = []
 
-    content = []
+  try :
+    ses.retrlines('MLSD %s'%(WDIR),content.append)
+  except :
+    print("\tError loading directories")
+    return {}
+
+  description = []
+
+  for l in content :
+    d = {}
+    for e in l.split(';'):
+      el = e.split('=')
+      if len(el) == 2:
+        d[el[0].strip()] = el[1].strip()
+      elif len(el) == 1:
+        d['name'] = el[0].strip()
+
+    description.append(d)
+
+  directories = [d['name'] for d in description if d['type'] == 'dir']
+ 
+  all_files = {}
+  for d in directories :
+    l_files = []
 
     try :
-        ses.retrlines('MLSD %s'%(WDIR),content.append)
+      ses.retrlines('NLST %s%s/'%(WDIR,d), l_files.append)
     except :
-        print("\tError loading directories")
-        return {}
+      print("\tError loading directory")
 
-    description = []
+    if len(l_files) > 0 :
 
-    for l in content :
-        d = {}
-        for e in l.split(';'):
-            el = e.split('=')
-            if len(el) == 2:
-                d[el[0].strip()] = el[1].strip()
-            elif len(el) == 1:
-                d['name'] = el[0].strip()
+      all_files[d] = [re.findall('.*?(\w+\.js)$',f)[0] for f in l_files if len(re.findall('.*?(\w+\.js)$',f)) > 0]
+ 
+  return all_files
 
-        description.append(d)
+def list_schematics():
 
-    directories = [d['name'] for d in description if d['type'] == 'dir']
-   
-    all_files = {}
-    for d in directories :
-        l_files = []
+  print("\tFTP list schematics")
 
-        try :
-            ses.retrlines('NLST %s%s/'%(WDIR,d), l_files.append)
-        except :
-            print("\tError loading directory")
+  ses = getSession()
 
-        if len(l_files) > 0 :
-            
-            all_files[d] = [re.findall('.*?(\w+\.js)$',f)[0] for f in l_files if len(re.findall('.*?(\w+\.js)$',f)) > 0]
-   
-    return all_files
+  if ses is None :
+    return {}
+
+  content = []
+
+  try :
+    ses.retrlines('MLSD %s'%(SCHEM),content.append)
+  except :
+    print("\tError loading directories")
+    return {}
+
+  description = []
+
+  for l in content :
+    d = {}
+    for e in l.split(';'):
+      el = e.split('=')
+      if len(el) == 2:
+        d[el[0].strip()] = el[1].strip()
+      elif len(el) == 1:
+        d['name'] = el[0].strip()
+
+    description.append(d)
+
+  files = [d['name'] for d in description if d['type'] == 'file']
+
+  files = [re.findall('.*?(\w+\.schematic)$',f)[0] for f in files if len(re.findall('.*?(\w+\.schematic)$',f)) > 0]
+  
+  return files
 
 def save_file(playername,filename,txt) :
 
-    path = "%s%s/%s"%(WDIR,playername,filename)
+  path = "%s%s/%s"%(WDIR,playername,filename)
 
-    print("\tFTP save %s"%(path))
+  print("\tFTP save %s"%(path))
 
-    ses = getSession()
+  ses = getSession()
 
-    if ses is None :
-        return
+  if ses is None :
+      return
 
-    try :
-        ses.mkd(WDIR+playername)
-    except:
-        print('\tFTP dir %s already exists'%(playername))
+  try :
+    ses.mkd(WDIR+playername)
+  except:
+    print('\tFTP dir %s already exists'%(playername))
 
-    try:
-        with open('tempfile', 'wb') as f : f.write(txt.encode('utf8'))
-        ses.storbinary('STOR %s'%(path), open('tempfile','rb'))
-    except:
-        print("\tFTP: ERROR SAVING FILE %s"%path)
+  try:
+    with open('tempfile', 'wb') as f :
+      f.write(txt.encode('utf8'))
 
+    ses.storbinary('STOR %s'%(path), open('tempfile','rb'))
+
+  except:
+    print("\tFTP: ERROR SAVING FILE %s"%path)
+
+  return
+
+
+def save_schematic(filename, mfile) :
+
+  path = "%s%s"%(SCHEM,filename)
+
+  print("\tFTP save %s"%(path))
+
+  ses = getSession()
+
+  if ses is None :
     return
+
+  try:
+    
+    ses.storbinary('STOR %s'%(path), mfile)
+
+  except:
+    print("\tFTP: ERROR SAVING FILE %s"%path)
+
+  return
+
 
 def load_file(playername,filename):
 
-    path = "%s%s/%s"%(WDIR,playername,filename)
+  path = "%s%s/%s"%(WDIR,playername,filename)
 
-    print("\tFTP load %s"%(path))
+  print("\tFTP load %s"%(path))
 
-    ses = getSession()
+  ses = getSession()
 
-    if ses is None :
-        return ""
+  if ses is None :
+    return ""
 
-    data = [] 
+  data = [] 
 
-    try:
-        res = ses.retrbinary('RETR %s'%(path), data.append)
-    except:
-        print("\tFTP ERROR LOADIND FILE %s"%path)
-        data = [""]
+  try:
+    res = ses.retrbinary('RETR %s'%(path), data.append)
+  except:
+    print("\tFTP ERROR LOADIND FILE %s"%path)
+    data = [""]
 
-    txt = ''.join(data).encode('utf8')
+  txt = ''.join(data).encode('utf8')
 
-    return txt 
+  return txt 
